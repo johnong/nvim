d43f512e0f45746550c82793f4ff53df7b3714f9
John Warren <john@warren.uk.net>
2025-11-19 08:20:47 +0000

    Start to add lsp setup

--- lua/john/lazy.lua
+++ lua/john/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("john.plugins", {
+require("lazy").setup({{import = "john.plugins"}, {import = "john.plugins.lsp"} }, {
   checker = {
     enabled = true,
     notify = false,
