1e80785f0b95717e99bcc8149603502dbc2b7160
John Warren <john@warren.uk.net>
2025-11-20 23:02:29 +0000

    Add formatting and linting

--- lua/john/plugins/lsp/mason.lua
+++ lua/john/plugins/lsp/mason.lua
@@ -1,55 +1,58 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-       "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        "eslint",
-        "ruby_lsp",
-        "pico8_ls",
-        "pylsp",
-      },
-    },
-    dependencies = {
-      {
-        "mason-org/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        },
-      },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    },
-    dependencies = {
-      "mason-org/mason.nvim",
-    },
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers for mason to install
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"eslint",
+				"ruby_lsp",
+				"pico8_ls",
+				"pylsp",
+			},
+		},
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+				"rubocop", -- Ruby formatter
+				"erb-formatter", -- Rails
+				"rubyfmt",
+			},
+		},
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+	},
 }
